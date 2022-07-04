86be7bb700b1252f7c7577a3f43dbcda1920040c
Tyler Correu <60193729+Correu@users.noreply.github.com>
2022-07-04 18:57:33 -0500

    controller updates and button addition

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,9 +2,18 @@ package sample;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 
+import java.io.IOException;
+
 public class Controller {
 
     public void testPDFBox() {
         PDDocument doc = new PDDocument();
+
+        try {
+            doc.save("fileName");
+            doc.close();
+        } catch (IOException ioe) {
+            System.out.print(ioe.getMessage());
+        }
     }
 }
